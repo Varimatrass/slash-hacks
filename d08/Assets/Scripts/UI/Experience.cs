aee1901568b6ce8e0f8445f08eacc1196448cb56
mantal <mantal0@outlook.fr>
2017-01-20 18:23:38 +0100

    Add HUD

---
+++ d08/Assets/Scripts/UI/Experience.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Experience : MonoBehaviour {
+
+	public Creature Player;
+
+	private Slider Slider;
+
+	private void Start() {
+		Slider = GetComponent<Slider> ();
+	}
+
+	void Update () {
+		Slider.value = Player.Experience;
+		Slider.maxValue = Player.ExperienceToNextLevel;
+	}
+}
