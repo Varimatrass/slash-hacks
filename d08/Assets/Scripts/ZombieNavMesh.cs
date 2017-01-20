1547f30c678452d4ea4d97330b7a06214dd9b015
Mickael SORIN <msorin@e1r13p4.42.fr>
2017-01-20 15:12:35 +0100

    Ajout du script zombie

---
+++ d08/Assets/Scripts/ZombieNavMesh.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieNavMesh : MonoBehaviour {
+
+	[HideInInspector]
+	public Maya maya;
+
+	[HideInInspector]
+	public bool dead = false;
+
+	private bool focus;
+
+	// Use this for initialization
+	void Start () {
+		focus = false;
+	}
+
+	void OnTriggerEnter (Collider col) {
+		if (col.name == "Maya") {
+			maya = col.GetComponent<Maya> ();
+			focus = true;
+		}
+	}
+
+	void BuriedAgain () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (dead)
+			BuriedAgain ();
+		else if (focus) {
+			this.GetComponent<NavMeshAgent> ().destination = maya.transform.position;
+		}
+	}
+}
